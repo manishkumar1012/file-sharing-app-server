59ab6a8b2c2feff31e7bf5e43b686fbb3dfae66f
Manish Kumar Rajak <manishkumar.jnv12@gmail.com>
2021-04-11 22:43:23 +0530

    Routing setup

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 3000;
 const connectDB = require('./config/db');
 connectDB();
 
+app.use('/api/files', './routes/files.js');
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
