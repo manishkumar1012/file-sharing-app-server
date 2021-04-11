b06fef9eee954c61780ef9944cda7ca462f39229
Manish Kumar Rajak <manishkumar.jnv12@gmail.com>
2021-04-11 23:12:17 +0530

    multer setup for uploading files

--- routes/files.js
+++ routes/files.js
@@ -1,7 +1,25 @@
 const router = require('express').Router();
+const multer = require('multer');
+const path = require('path');
 
-router.post('/', () => {
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, '../uploads'),
+    filename: (req, file, cb) => {
+        const uniqueName = `${Date.now()}-${Math.round(
+            Math.random() * 1e9
+        )}${path.extname(file.originalname)}`; // like 234453446-343435344.jpg
 
+        cb(null, uniqueName);
+    },
 });
+
+const upload = multer
+    .upload({
+        storage,
+        limit: { fileSize: 1e6 * 10 },
+    })
+    .single('myfile');
+
+router.post('/', (req, res) => {});
 
 module.exports = router;
