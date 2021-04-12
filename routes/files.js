cebe1d24da62f64fa21860605ec4f9571cfa0551
Manish Kumar Rajak <manishkumar.jnv12@gmail.com>
2021-04-12 15:45:35 +0530

    file upload endpoint completed

--- routes/files.js
+++ routes/files.js
@@ -1,25 +1,39 @@
 const router = require('express').Router();
 const multer = require('multer');
 const path = require('path');
+const File = require('../models/file');
+const { v4: uuidv4 } = require('uuid');
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, '../uploads'),
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, 'uploads/'),
     filename: (req, file, cb) => {
         const uniqueName = `${Date.now()}-${Math.round(
             Math.random() * 1e9
-        )}${path.extname(file.originalname)}`; // like 234453446-343435344.jpg
-
+        )}${path.extname(file.originalname)}`;
         cb(null, uniqueName);
     },
 });
 
-const upload = multer
-    .upload({
-        storage,
-        limit: { fileSize: 1e6 * 10 },
-    })
-    .single('myfile');
+let upload = multer({ storage, limits: { fileSize: 1000000 * 100 } }).single(
+    'myfile'
+); //100mb
 
-router.post('/', (req, res) => {});
+router.post('/', (req, res) => {
+    upload(req, res, async (err) => {
+        if (err) {
+            return res.status(500).send({ error: err.message });
+        }
+        const file = new File({
+            filename: req.file.filename,
+            uuid: uuidv4(),
+            path: req.file.path,
+            size: req.file.size,
+        });
+        const response = await file.save();
+        res.json({
+            file: `${process.env.APP_BASE_URL}/files/${response.uuid}`,
+        });
+    });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const connectDB = require('./config/db');
 connectDB();
 
-app.use('/api/files', './routes/files.js');
+app.use('/api/files', require('./routes/files.js'));
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
