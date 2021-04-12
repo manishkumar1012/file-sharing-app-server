474b5a9e16d6418cd32a4fe32c72d2c0edbbcd96
Manish Kumar Rajak <manishkumar.jnv12@gmail.com>
2021-04-12 14:36:01 +0530

    created file schema

---
+++ models/file.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const fileSchema = new mongoose.Schema({
+    filename: { type: String, required: true },
+    path: { type: String, required: true },
+    size: { type: Number, required: true },
+    uuid: { type: String, required: true },
+    sender: { type: String, required: false },
+    receiver: { type: String, required: false },
+}, { timestamps: true });
+
+module.exports = new mongoose.model('File', fileSchema);
