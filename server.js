2c8feadd8ee0177948c6547ac4b1a6b793fe78d4
Manish Kumar Rajak <manishkumar.jnv12@gmail.com>
2021-04-13 11:05:51 +0530

    API to display download page

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Database
 const connectDB = require('./config/db');
 connectDB();
 
-app.use('/api/files', require('./routes/files.js'));
+// Template engine
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+// Routes
+app.use('/api/files', require('./routes/files'));
+app.use('/files', require('./routes/show'));
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
