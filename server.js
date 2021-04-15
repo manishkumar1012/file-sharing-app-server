f13431817ea6474957c9662edc1d93f9d4d7cc7f
Manish Kumar Rajak <manishkumar.jnv12@gmail.com>
2021-04-15 15:25:18 +0530

    added cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
@@ -10,6 +11,12 @@ const connectDB = require('./config/db');
 connectDB();
 
 app.use(express.json());
+
+// Cors
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+}
+app.use(cors(corsOptions));
 
 // Template engine
 app.set('views', path.join(__dirname, '/views'));
